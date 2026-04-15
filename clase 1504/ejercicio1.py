d59c4c92a8c9411b190dfbfc7960012e1de49451
HuilquiFigueroa <hfgamerpro23@gmail.com>
2026-04-15 17:48:44 -0300

    Add files via upload
    ejercicio 1

---
+++ clase 1504/ejercicio1.py
@@ -0,0 +1,10 @@
+numero= 12
+numero_decimal = 14.93
+nombre_numero = "doce"
+numerovof = False
+print(type(numero))
+print(type(numero_decimal))
+print(type(nombre_numero))
+print(type(numerovof))
+
+
